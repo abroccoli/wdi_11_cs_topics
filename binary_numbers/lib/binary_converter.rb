b352afe47c7926aaa534096074fec640d2cf0811
abroccoli <acbrocco@syr.edu>
2015-03-30 10:52:03 -0400

    wrote code for binary string to integer with base 10

--- binary_numbers/lib/binary_converter.rb
+++ binary_numbers/lib/binary_converter.rb
@@ -1,4 +1,8 @@
 module BinaryConverter
-  def to_fixnum(binary_string)
+  def self.to_fixnum(binary_string)
+    # binary_string.to_i(2)
+    result = []
+    binary_string.split('').reverse.each_with_index{|x, i| result << (x == '1' ? (2 ** i) : 0)}
+    return result.inject(&:+)
   end
 end
